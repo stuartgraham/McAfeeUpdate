e25774acdd153e271ffca4896400136492715a4a
Stuart Graham <Stuart Graham>
2016-12-24 23:55:29 +1030

    Major simplication of code

--- README.md
+++ README.md
@@ -10,8 +10,8 @@ or<br>
 <br>
 <b>Prepare Python environment</b><br>
 <code>python --version</code>                       #should respond with 'Python 3.x.x'<br>
-<code>python -m pip install requests</code>         #should respond with 'Successfully installed requests-2.x.x'<br>
-<code>python -m pip install BeautifulSoup4</code>   #should respond with 'Successfully installed BeautifulSoup4-x.x.x'<br>
+<code>pip install -r requirements.txt</code>         #should respond with 'Successfully installed BeautifulSoup4-x.x.x requests-2.x.x'<br>
+<br> 
 <br>
 <b>Execution</b><br>
 <code>python main.py</code>
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+"""Grab HTTP directory from McAfee site and download directory structure"""
+
 import os
 import logging
 import time
-import defgrab
-import processfile
+import hashlib
+from bs4 import BeautifulSoup
+import requests
 import config
 
 #Logging config
@@ -15,82 +18,255 @@ logging.basicConfig(filename=('logs/updater'+ time.strftime("%d%m%Y") +'.log'),
                     format=FORMAT, datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO)
 LOGGER = logging.getLogger(__name__)
 
-# Define link list
-linkdb = []
 
-def processsoup(soup, baseurl):
-    """Process fully qualifed links"""
-    for line in soup:
-        linkdb.append(baseurl + line)
-
-def roothttp():
-    """Resolve root directory links"""
-    rootsoup = defgrab.linkdir()
-    LOGGER.info("Processing root HTTP directory")
-    processsoup(rootsoup[0], rootsoup[1])
-
-def childhttp():
-    """Resolve child diretory links"""
-    childsoupcounter = 0
-    for tempuri in linkdb:
-        if tempuri[-1] == '/':
-            childsoupcounter += 1
-            childsoup = defgrab.linkdir(tempuri)
-            LOGGER.info("Processing child HTTP directory " + str(childsoupcounter) + " " + tempuri)
-            processsoup(childsoup[0], childsoup[1])
-
-# Process file from linkdb
-def linksprocess(linkdb):
-    """Process file from linkdb"""
-    for dluri in linkdb:
-        if not dluri[-1] == '/':
-            LOGGER.info("Processing " + dluri)
-            processfile.go(dluri)
-
-# Log linkdb creation
-# Test for Logs Dir and existing files, rectify as needed
-def loglinkdb(linkdb):
-    """Log linkdb creation. Test for Logs Dir and existing files, rectify as needed"""
-    LOGGER.info("Processing linkdb log/n/n")
-    LOGGER.info("\n".join(linkdb))
-
-# Function will walk the filesystem and remove old files and folders
-# as defined in the retention setting in config.py
-def purgeold(retention=config.retention, rootdir=config.destinationpath):
-    LOGGER.info("Starting retention cleanup process")
-    retentionbuffer = float(0.5)
-    retention = retention + retentionbuffer
-    LOGGER.info("Retention is set to " + str(retention) + " days, rention setting + 0.5 day buffer")
+def purgeold(retention=config.RETENTION, rootdir=config.DESTINATIONPATH):
+    """Function will walk the filesystem and remove old files and
+    folders as defined in the retention setting in config.py"""
+    if rootdir == 'logs':
+        LOGGER.info("RETENTIONINFO : Starting retention cleanup process for logs")
+    else:
+        LOGGER.info("RETENTIONINFO : Starting retention cleanup process for files")
+    retention += float(0.5)
+    LOGGER.info("RETENTIONINFO : Retention is set to " + str(retention) +
+                " days, rention setting + 0.5 day buffer")
+    deleteddirs = 0
+    deletedfiles = 0
     for root, dirs, files in os.walk(rootdir):
         if dirs == [] and files == []:
             os.removedirs(root)
-            LOGGER.info(root + " was empty and has been deleted")
+            deleteddirs += 1
+            LOGGER.info("RETENTIONDELETE : " + root + " was empty and has been deleted")
 
         for i in files:
             path = (root+"\\"+i)
-            timestamp = os.stat(path)
-            createsecs = timestamp.st_ctime
-            templocaltime = time.localtime()
-            currentsecs = time.mktime(templocaltime)
-            deltasecs = currentsecs - createsecs
-            deltadays = float(deltasecs/86400)
-            deltadays = round(deltadays, 3)
+            deltadays = timecalc(os.stat(path))
             if deltadays > retention:
                 try:
                     os.remove(path)
-                    LOGGER.info("RETENTIONDEL: " + path + " was " + str(deltadays) + " days old and deleted")
+                    deletedfiles += 1
+                    LOGGER.info("RETENTIONDELETE: " + path + " was " + str(deltadays) +
+                                " days old and deleted")
                 except OSError as err:
                     LOGGER.info("OSerror" + err)
             else:
-                LOGGER.info("RETENTIONKEEP: " + path + " is " + str(deltadays) + " days old and is retained")
+                LOGGER.info("RETENTIONKEEP: " + path + " is " + str(deltadays) +
+                            " days old and is retained")
+
+    LOGGER.info("RETENTIONSTATS : " + str(deleteddirs) + " directories were deleted")
+    LOGGER.info("RETENTIONSTATS : " + str(deletedfiles) + " files were deleted")
+
+
+def soupgen(path=config.SOURCEPATH):
+    """Send URI string to requests module for resolution, if HTTP 200 pass to BeautifulSoup
+     module for cleansing links and remove junk links (not files or dirs)"""
+    resp = requests.get(url=path, proxies=config.PROXY, timeout=config.HTTPTIMEOUT)
+    if not resp.status_code == 200:
+        LOGGER.debug("DEBUG : Page not found. Fix the sourcepath setting in config.py.")
+    else:
+        soup = BeautifulSoup(resp.text, 'html.parser')
+        prunedsoup = removejunklinks(soup)
+        baseurl = path
+        return prunedsoup, baseurl
+
+
+def constructlinkslist():
+    """Construct link list"""
+    linkdump = soupgen()
+    linkslist = []
+    # Import cleaned links for root directory to list
+    LOGGER.info("DIRPROCESS : Processing root HTTP directory")
+    for line in linkdump[0]:
+        linkslist.append(linkdump[1] + line)
+    # Import cleaned links for child direcories to list
+    childsoupcounter = 0
+    for tempuri in linkslist:
+        if tempuri[-1] == '/':
+            childsoupcounter += 1
+            linkdump = soupgen(tempuri)
+            LOGGER.info("DIRPROCESS : Processing child HTTP directory " +
+                        str(childsoupcounter) + " " + tempuri)
+            for line in linkdump[0]:
+                linkslist.append(linkdump[1] + line)
+    return linkslist
+
+
+def loglinkdb(linkslist):
+    """Log linkdb creation. Test for Logs Dir and existing files, rectify as needed"""
+    LOGGER.info("DIRPROCESS : Dump log of links processed")
+    LOGGER.info("\n".join(linkslist))
+
+
+def timecalc(timestamp):
+    """Converts timestamp to seconds, does local conversion
+    and defines delta between now and timestamp"""
+    createsecs = timestamp.st_ctime
+    templocaltime = time.localtime()
+    currentsecs = time.mktime(templocaltime)
+    deltasecs = currentsecs - createsecs
+    deltadays = float(deltasecs/86400)
+    deltadays = round(deltadays, 3)
+    return deltadays
+
+
+def removejunklinks(soup):
+    """Removes irrelevant links"""
+    output = []
+    for link in soup.find_all('a'):
+        # drop the sorting and formatting links
+        if '?' in link.get('href'):
+            continue
+        # drop the parent directory links
+        elif '..' in link.get('href'):
+            continue
+        else:
+            worklink = (link.get('href'))
+            output.append(worklink)
+    return output
+
+
+def checkdestpath(destinationpath=config.DESTINATIONPATH):
+    """Module will check existance of the base destination path from config and create as needed"""
+    if not os.path.exists(destinationpath):
+        try:
+            os.makedirs(destinationpath)
+            LOGGER.info("DIRCREATE: " + destinationpath + " was created")
+        except OSError as err:
+            LOGGER.info(err)
+        else:
+            LOGGER.info("DIRSKIP: " + destinationpath + " already exists, skipping mkdir")
+    #Create anchor file to stop deletion
+    anchorfile = (destinationpath + '\\anchor.txt')
+    if not os.path.exists(anchorfile):
+        with open(anchorfile, 'a'):
+            os.utime(anchorfile, None)
+    else:
+        os.remove(anchorfile)
+        with open(anchorfile, 'a'):
+            os.utime(anchorfile, None)
+
+
+def makedir(path, destinationpath=config.DESTINATIONPATH):
+    """Test and create the directory structure for the file as needed"""
+    # Prep the mkpath
+    dircreated = False
+    if "/" not in path:
+        mkpath = (destinationpath)
+        filename = path
+    elif path == '':
+        mkpath = ''
+        filename = ''
+    else:
+        directory = path.split("/")
+        filename = directory[-1]
+        directory.pop(-1)
+        workingpath = "\\".join(directory)
+        mkpath = (destinationpath + "\\" + workingpath)
+        # Execute makedirs to build directors
+        try:
+            os.makedirs(mkpath)
+            LOGGER.info("DIRCHECKCREATE: " + mkpath + " was created")
+            dircreated = True
+        except OSError:
+            LOGGER.info("DIRCHECKSKIP: " + mkpath + " already exists, skipping mkdir")
+    return mkpath, filename, dircreated
+
+
+def checkfile(writepath, dluri):
+    """Inspect the files upstream header tags for md5 hash and compare
+    before sending for download as required"""
+    if os.path.exists(writepath):
+        try:
+            hasher = hashlib.md5()
+            with open(writepath, 'rb') as tempfile:
+                buf = tempfile.read()
+                hasher.update(buf)
+                downstreammd5 = hasher.hexdigest()
+
+            for _ in range(config.MAXRETRIES):
+                try:
+                    resp = requests.head(url=dluri, proxies=config.PROXY,
+                                         timeout=config.HTTPTIMEOUT)
+                    upstreammd5 = resp.headers['ETag'].split(":", 1)[0]
+                    upstreammd5 = upstreammd5[1:]
+                    LOGGER.info("HASHCALC : Downstream MD5 Hash : " + downstreammd5)
+                    LOGGER.info("HASHCALC : Upstream MD5 Hash   : " + upstreammd5)
+                    resp.close()
+                    break
+                except requests.exceptions.Timeout:
+                    LOGGER.error("TIMEOUT : Connecion timed out gathering the header")
+            if upstreammd5 == downstreammd5:
+                LOGGER.info("HASHMATCH: " + writepath + " MD5 match")
+                dlreq = 0
+            else:
+                LOGGER.info("HASHMISMATCH : " + writepath +
+                            " MD5 didnt match, progressing to download")
+                dlreq = 1
+        except KeyError as err:
+            logging.error(err)
+            dlreq = 1
+    else:
+        LOGGER.info("FILECHECK : " + writepath + " this was not detected, sending for download")
+        dlreq = 1
+    return dlreq
+
+
+def dlfile(dlreq, writepath, dluri):
+    """Download file when dlreq is passed in as 1"""
+    if dlreq == 1:
+        for _ in range(config.MAXRETRIES):
+            try:
+                resp = requests.get(url=dluri, proxies=config.PROXY,
+                                    stream=True, timeout=config.HTTPTIMEOUT)
+                LOGGER.info("DOWNLOADFILE : Downloading " + dluri + " to " + writepath)
+                with open(writepath, 'wb') as tempfile:
+                    for chunk in resp.iter_content(chunk_size=1024):
+                        if chunk:
+                            tempfile.write(chunk)
+                resp.close()
+                break
+            except requests.exceptions.Timeout:
+                LOGGER.error("TIMEOUT : Connection timed out downloading the file")
+
+        LOGGER.info("DOWNLOADCOMPLETE: " + writepath + " completed, sending for MD5 verification")
+        checkfile(writepath, dluri)
+
+
+def process(links):
+    """Create folder structure and download files for URIs passed to it"""
+    dirscreated = 0
+    filescreated = 0
+    for dluri in links:
+        if not dluri[-1] == '/':
+            LOGGER.info("LINKPROCESS : Processing " + dluri)
+            path = dluri.replace(config.SOURCEPATH, "")
+            temppath = makedir(path)
+            # Incremenent the created directory counter
+            if temppath[2]:
+                dirscreated += 1
+            # Decide how to download file
+            if temppath[0] and temppath[1] == '':
+                LOGGER.info("NOURL : Blank URI skipping")
+            elif temppath[0] == "\\":
+                writepath = temppath[0] + temppath[1]
+            else:
+                writepath = temppath[0] + "\\" + temppath[1]
+            dlreq = checkfile(writepath, dluri)
+            dlfile(dlreq, writepath, dluri)
+            if dlreq == 1:
+                filescreated += 1
+
+    LOGGER.info("DOWNLOADSTATS : " + str(dirscreated) + " directories were created")
+    LOGGER.info("DOWNLOADSTATS : " + str(filescreated) + " files were downloaded")
+
 
 # Main execution
-processfile.checkdestpath()
-purgeold()
-roothttp()
-childhttp()
-linksprocess(linkdb)
-loglinkdb(linkdb)
-LOGGER.info("****PASS COMPLETED****")
-#Purge old logs
-purgeold(30, 'logs')
+if __name__ == "__main__":
+    LOGGER.info("###### PASS STARTED ######")
+    checkdestpath()
+    purgeold()
+    LINKSLIST = constructlinkslist()
+    process(LINKSLIST)
+    loglinkdb(LINKSLIST)
+    #Purge old logs
+    purgeold(30, 'logs')
+    LOGGER.info("****** PASS COMPLETED ******")
